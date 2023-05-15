a3be4c19b03cd3135b32561705a155214f26f2db
aylaram <43983561+aylaram@users.noreply.github.com>
2023-05-16 00:12:45 +0300

    Update DishRow.js

--- components/DishRow.js
+++ components/DishRow.js
@@ -36,7 +36,7 @@ const DishRow = ({ id, name, description, price, image }) => {
             <Text className="text-lg mb-1 font-bold">{name}</Text>
             <Text className="text-gray-400 text-xs">{description}</Text>
             <Text className="text-gray-400 mt-2 ">
-              <Currency quantity={price} currency="INR" />
+              <Currency quantity={price} currency="EUR" />
             </Text>
           </View>
           <View>
